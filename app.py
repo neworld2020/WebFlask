99f4b2bc6346cff777e25d4f19e6d7e61299d3a7
Song Mengxuan <neworld2020@126.com>
2023-01-03 21:45:00 +0800

    API测试

--- app.py
+++ app.py
@@ -8,5 +8,38 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+"""
+用户登录及注册相关接口
+"""
+
+"""
+语料库获取相关，需要用户已登录并已获得UserKey，以后所有数据库获取都需要UserKey
+"""
+
+
+@app.route('/vocabulary', methods=['GET'])
+def get_vocabulary():
+    # args = request.args
+    # # get UserKey from args
+    # user_key = args.get('UserKey')
+    # user = get_user_by_key(user_key)
+    # # get suitable vocabulary for user
+    # vocabulary = get_vocabulary_for_user(user)
+    # return vocabulary
+    return {'type': 'vocabulary'}, 200, {'ContentType': 'application/json'}
+
+
+@app.route('/word-details', methods=['GET'])
+def get_word_details():
+    # args = request.args
+    # # get UserKey from args
+    # user_key = args.get('UserKey')
+    # user = get_user_by_key(user_key)
+    # # get suitable vocabulary for user
+    # vocabulary = get_word_details_for_user(user)
+    # return vocabulary
+    return {'type': 'word-details'}, 200, {'ContentType': 'application/json'}
+
+
 if __name__ == '__main__':
     app.run()
